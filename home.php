0e7b259b5c72e0faa4b4e5a2b91461ca6c918ceb
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2024-12-14 12:48:14 +0600

    php variable and data type complete

--- home.php
+++ home.php
@@ -1,15 +1,53 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <p>bangladesh</p>
 
 <?php
-?>
 
-</body>
-</html>
+// COMMENT
+
+/*
+commment 
+COMMENT
+comment
+*/
+
+
+// PHP Var
+// PHP data typs
+// PHP ERORR
+$userName = "Jhon Deo";
+$name = "Fatema";
+
+ //echo $userName;
+
+// const userame = "Jone Deo";
+
+// echo $userame;
+
+ $number = 1;
+ $number2 = 1.6;
+ $isEnrol = true; 
+ $students = [
+  "habib", "jubayer"
+ ];
+$arry = array( "habib", "jubayer");
+
+$nullValue = null;
+
+
+
+// var_dump( value:  $isEnrol );
+
+$a = 10;
+$b = 11;
+$c = 12;
+$d = 13;
+
+$e = $a + $b +$c + $d;
+//  echo  $e;
+
+
+var_dump(value: $e)
+
+
+
+
+?>
