e6c87d1027f1bddf5a868c9bb9e00c986bc5aef2
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2025-01-12 16:43:28 +0600

    update file

---
+++ form_validation/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
+
+<form action="submit.php" method="post">
+    <input type="text" name='username' placeholder="Name"><br/><br/>
+		<input type="text" name='email' placeholder="Email"><br/><br/>
+		<input type="text" name='password' placeholder="Password"><br/><br/>
+		<button type="submit">Submit Data</button>
+</form>
+
+
+
+
+</body>
+</html>
