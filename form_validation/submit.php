3cac9120c8487ec3bc6fa6384b7ad612b13f8ce0
abdullad2bd <abdullahalmaruf2bd@gmail.com>
2025-01-12 17:17:30 +0600

    complete

--- form_validation/submit.php
+++ form_validation/submit.php
@@ -1,46 +1,78 @@
 <?php
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// connect to the database
-$host = 'localhost';  
-$user = 'root';
-$pass = '';
-$db = 'users';
-$conn = mysqli_connect($host, $user, $pass, $db); 
-
-$name = $_REQUEST['name'];
-$email = $_REQUEST['email'];
-$password = $_REQUEST['password']; 
-
-$sql = "INSERT INTO submitform (name, email, password) VALUES ('$name ', '$email', '$password')";  
-$data = mysqli_query($conn, $sql);
-
-if($data){
-    echo "Data inserted into database";
+$has_error = false;
+$errors = array();  
+// Request validation
+$request_method = $_SERVER['REQUEST_METHOD'];
+if( 'POST' != $request_method ){
+	$has_error = true;
+	array_push($errors, 'Invalid request method!');
 }
-?>
+
+// Validate input fields
+$username = $_POST['name'];
+$email 	  = $_POST['email'];
+$password = $_POST['password'];
+
+// check username
+if( empty($username) ){
+	$has_error = true;
+	array_push($errors, 'Name cannot be empty!');
+}
+// check email
+if( empty($email) ){
+	$has_error = true;
+	array_push($errors, 'Email address cannot be empty!');
+}
+// check password
+if( empty($password) ){
+	$has_error = true;
+	array_push($errors, 'Password cannot be empty!');
+}
+
+// check password length
+if( strlen($password) < 5 ){
+	$has_error = true;
+	array_push($errors, 'Password length must be more than 4!');
+}
+
+
+
+// Display errors
+if($has_error){
+	foreach ($errors as $error) {
+		echo $error . "<br/>";
+	}
+}
+
+
+
+
+
+
+// Insert 
+if($has_error) {
+	die();
+}
+
+
+
+
+
+
+
+// database connection
+$host        = 'localhost';
+$db_user     = 'root';
+$db_password = '';
+$db_name     = 'usere';
+
+$conn = mysqli_connect($host, $db_user, $db_password, $db_name);
+
+if($conn){
+	$sql = "INSERT INTO submitform (name, email, password) VALUES ('$name', '$email', '$password')";
+	$insert = mysqli_query($conn, $sql);
+	if($insert){
+		echo "User inserted successfully!";
+	}
+
+}
